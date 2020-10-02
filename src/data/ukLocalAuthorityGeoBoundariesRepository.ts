72e34ed8093c57be7ca0e46c96b00a9f8048ccb7
Edonis Seferi <doni.seferi@gmail.com>
2020-10-02 23:18:55 +0100

    moved method up

--- src/data/ukLocalAuthorityGeoBoundariesRepository.ts
+++ src/data/ukLocalAuthorityGeoBoundariesRepository.ts
@@ -23,6 +23,11 @@ export default (
           : throwCoordinatesNotFoundError(latitude, longitude)
       );
 
+  return {
+    getAsync: async (latitude: number, longitude: number) =>
+      await getAsync(latitude, longitude),
+  };
+
   const validate = (entity: UKLocalAuthorityResult[]) =>
     entity && entity.length === 1;
 
@@ -40,11 +45,6 @@ export default (
           ` the boundaries of the United Kingdom.`
       );
     })();
-
-  return {
-    getAsync: async (latitude: number, longitude: number) =>
-      await getAsync(latitude, longitude),
-  };
 };
 
 export type UKLocalAuthority = {
