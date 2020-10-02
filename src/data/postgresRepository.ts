520bfcada5a0d46c7950bb95142217cd28922403
Edonis Seferi <doni.seferi@gmail.com>
2020-10-02 22:35:03 +0100

    repo typo

--- src/data/postgresRepository.ts
+++ src/data/postgresRepository.ts
@@ -1,7 +1,7 @@
 import { Client, ClientConfig } from "pg";
 
 export default (config: string | ClientConfig) => {
-  const executeAync = async <T>(command: {
+  const executeAsync = async <T>(command: {
     text: string;
     values: string[];
   }): Promise<T[]> => {
@@ -9,15 +9,18 @@ export default (config: string | ClientConfig) => {
     return await client
       .connect()
       .then(
-        async () => await client.query<T>(command).then((result) => result.rows)
+        async () =>
+          await client
+            .query<T>({ name: "geofunction", ...command })
+            .then((result) => result.rows)
       )
       .finally(async () => client.end());
   };
 
   return {
-    executeAync: async <T>(command: {
+    executeAsync: async <T>(command: {
       text: string;
       values: string[];
-    }): Promise<T[]> => await executeAync(command),
+    }): Promise<T[]> => await executeAsync(command),
   };
 };
