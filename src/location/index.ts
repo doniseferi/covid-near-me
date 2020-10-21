bfc502abe41678563c53f478e4eea215f133fee0
doniseferi <doni.seferi@gmail.com>
2020-10-21 09:31:25 +0100

    refaq (#2)

--- src/location/index.ts
+++ src/location/index.ts
@@ -1,7 +1,7 @@
 import config from "../config/next.config";
 import postgresRepository from "./infastructure/postgresRepository";
 import localAuthorityRepository from "./repository/localAuthorityRepository";
-import { Location, LocalAuthorityRepository } from "./domain/localAuthority";
+import { Location, LocalAuthorityRepository } from "./interfaces/localAuthority";
 
 const locationRepository = localAuthorityRepository(
   postgresRepository(config.connectionString)
