97a6ec32404beb51f0fd67d4d6363bfd7a0fa754
Edonis Seferi <doni.seferi@gmail.com>
2020-11-03 21:56:33 +0000

    test updates

--- src/covid/index.test.ts
+++ src/covid/index.test.ts
@@ -1,59 +1,70 @@
 import { covidRepository } from "./index";
-import { mock } from "../test/util/httpMock";
+import polly from "../test/util/httpMock";
+import { Polly } from "@pollyjs/core";
 
-const polly = mock("covid");
-beforeAll(() => polly, 0);
-afterAll(async () => await polly.stop());
-test.concurrent.each`
+describe("Covid Repository", () => {
+  let mock: Polly;
+  beforeAll(
+    () =>
+      (mock = polly({
+        name: "covid",
+        mode: "replay",
+      }))
+  );
+  afterAll(() => mock.stop());
+  test.each`
     localAuthorityCode | newCasesPublished | newCasesBySpecimen | newDeathsPublished | rateOfCumulativeCasesBySpecimenDate | cumulativeCasesBySpecimen | rateOfCumulativeDeathsBySpecimen | cumulativeDeathsBySpecimen | newDeaths28DaysByPublishDate | cumulativeDeaths28DaysByPublishDate | cumulativeDeaths28DaysByPublishDateRate
     ${"N09000004"}     | ${79}             | ${59}              | ${0}               | ${788.5}                            | ${1142}                   | ${788.5}                         | ${1142}                    | ${0}                         | ${null}                             | ${null}
     ${"E09000033"}     | ${44}             | ${41}              | ${0}               | ${663.9}                            | ${1735}                   | ${663.9}                         | ${1735}                    | ${0}                         | ${134}                              | ${51.3}
     ${"W06000002"}     | ${16}             | ${11}              | ${0}               | ${778.7}                            | ${970}                    | ${778.7}                         | ${970}                     | ${0}                         | ${null}                             | ${null}
-    ${"S12000034"}     | ${8}              | ${9}              | ${0}               | ${312}                            | ${815}                    | ${312}                         | ${815}                     | ${0}                         | ${59}                               | ${22.6}
+    ${"S12000034"}     | ${8}              | ${9}               | ${0}               | ${312}                              | ${815}                    | ${312}                           | ${815}                     | ${0}                         | ${59}                               | ${22.6}
   `(
-  "location repository returns $expectedName for $latitude $longitude",
-  async ({
-    localAuthorityCode,
-    newCasesPublished,
-    newCasesBySpecimen,
-    newDeathsPublished,
-    rateOfCumulativeCasesBySpecimenDate,
-    cumulativeCasesBySpecimen,
-    rateOfCumulativeDeathsBySpecimen,
-    cumulativeDeathsBySpecimen,
-    newDeaths28DaysByPublishDate,
-    cumulativeDeaths28DaysByPublishDate,
-    cumulativeDeaths28DaysByPublishDateRate,
-  }) => {
-    const result = await covidRepository.getAsync(
-      {
-        code: localAuthorityCode,
-        name: "abc",
-        geoCoordinates: { latitude: 1, longitude: 1 },
-      },
-      new Date(Date.UTC(2020, 9, 14))
-    );
-    expect(result.newCasesPublished).toEqual(newCasesPublished);
-    expect(result.newCasesBySpecimen).toEqual(newCasesBySpecimen);
-    expect(result.newDeathsPublished).toEqual(newDeathsPublished);
-    expect(result.rateOfCumulativeCasesBySpecimenDate).toEqual(
-      rateOfCumulativeCasesBySpecimenDate
-    );
-    expect(result.cumulativeCasesBySpecimen).toEqual(cumulativeCasesBySpecimen);
-    expect(result.rateOfCumulativeDeathsBySpecimen).toEqual(
-      rateOfCumulativeDeathsBySpecimen
-    );
-    expect(result.cumulativeDeathsBySpecimen).toEqual(
-      cumulativeDeathsBySpecimen
-    );
-    expect(result.newDeaths28DaysByPublishDate).toEqual(
-      newDeaths28DaysByPublishDate
-    );
-    expect(result.cumulativeDeaths28DaysByPublishDate).toEqual(
-      cumulativeDeaths28DaysByPublishDate
-    );
-    expect(result.cumulativeDeaths28DaysByPublishDateRate).toEqual(
-      cumulativeDeaths28DaysByPublishDateRate
-    );
-  }
-);
+    "returns covid data for local authorities",
+    async ({
+      localAuthorityCode,
+      newCasesPublished,
+      newCasesBySpecimen,
+      newDeathsPublished,
+      rateOfCumulativeCasesBySpecimenDate,
+      cumulativeCasesBySpecimen,
+      rateOfCumulativeDeathsBySpecimen,
+      cumulativeDeathsBySpecimen,
+      newDeaths28DaysByPublishDate,
+      cumulativeDeaths28DaysByPublishDate,
+      cumulativeDeaths28DaysByPublishDateRate,
+    }) => {
+      const result = await covidRepository.getAsync(
+        {
+          code: localAuthorityCode,
+          name: "abc",
+          geoCoordinates: { latitude: 1, longitude: 1 },
+        },
+        new Date(Date.UTC(2020, 9, 14))
+      );
+      expect(result.newCasesPublished).toEqual(newCasesPublished);
+      expect(result.newCasesBySpecimen).toEqual(newCasesBySpecimen);
+      expect(result.newDeathsPublished).toEqual(newDeathsPublished);
+      expect(result.rateOfCumulativeCasesBySpecimenDate).toEqual(
+        rateOfCumulativeCasesBySpecimenDate
+      );
+      expect(result.cumulativeCasesBySpecimen).toEqual(
+        cumulativeCasesBySpecimen
+      );
+      expect(result.rateOfCumulativeDeathsBySpecimen).toEqual(
+        rateOfCumulativeDeathsBySpecimen
+      );
+      expect(result.cumulativeDeathsBySpecimen).toEqual(
+        cumulativeDeathsBySpecimen
+      );
+      expect(result.newDeaths28DaysByPublishDate).toEqual(
+        newDeaths28DaysByPublishDate
+      );
+      expect(result.cumulativeDeaths28DaysByPublishDate).toEqual(
+        cumulativeDeaths28DaysByPublishDate
+      );
+      expect(result.cumulativeDeaths28DaysByPublishDateRate).toEqual(
+        cumulativeDeaths28DaysByPublishDateRate
+      );
+    }
+  );
+});
--- src/test/util/httpMock.ts
+++ src/test/util/httpMock.ts
@@ -1,21 +1,25 @@
 import { Polly, Timing } from "@pollyjs/core";
 import NodeHttpAdapter from "@pollyjs/adapter-node-http";
 import FsPersister from "@pollyjs/persister-fs";
-import { MODE } from "@pollyjs/core";
 
-const mockHttp = (name: string, mode: MODE): Polly => {
+type MockHttpConfig = {
+  name: string;
+  mode: "record" | "replay";
+  delayInMilliseconds?: number;
+};
+
+const mockHttp = (config: MockHttpConfig): Polly => {
   Polly.register(NodeHttpAdapter);
   Polly.register(FsPersister);
-  return new Polly(name, {
-    mode,
+  return new Polly(config.name, {
+    mode: config.mode,
     adapters: ["node-http"],
     persister: "fs",
     logging: false,
-    recordFailedRequests: mode === "record",
-    recordIfMissing: mode === "record",
+    recordFailedRequests: true,
+    recordIfMissing: true,
+    timing: Timing.fixed(config.delayInMilliseconds ?? 0),
   });
 };
-const record = (name: string): Polly => mockHttp(name, "record");
-const mock = (name: string): Polly => mockHttp(name, "replay");
 
-export { record, mock };
+export default (config: MockHttpConfig): Polly => mockHttp(config);
