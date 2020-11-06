6749638cfe84408830ea8b529cb0e876986bb632
Edonis Seferi <doni.seferi@gmail.com>
2020-11-06 21:53:41 +0000

    port

--- jest.env-setup.js
+++ jest.env-setup.js
@@ -1,7 +1,7 @@
 process.env.POSTGRES_USER = "postgres";
 process.env.POSTGRES_PASSWORD = "password";
 process.env.POSTGRES_HOST = "localhost";
-process.env.POSTGRES_PORT = "5432";
+process.env.POSTGRES_PORT = "25432";
 process.env.POSTGRES_DB = "postgres";
 process.env.POSTGRES_TIMEOUT_IN_SECONDS = "10";
 process.env.POSTGRES_COMMAND_TIMEOUT = "10";
