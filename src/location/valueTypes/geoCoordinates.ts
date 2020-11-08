a42de206aed83ed9e776223f4a3fe925e533d3e8
Edonis Seferi <doni.seferi@gmail.com>
2020-11-08 22:58:10 +0000

    geo refaq

--- src/location/valueTypes/geoCoordinates.ts
+++ src/location/valueTypes/geoCoordinates.ts
@@ -3,13 +3,6 @@ export interface GeoCoordinates {
   longitude: number;
 }
 
-export default (latitude: number, longitude: number): GeoCoordinates =>
-  latitude < minimumLatitude || latitude > maximumLatitude
-    ? throwLatitudeError(latitude)
-    : longitude < minimumLongitude || longitude > maximumLongitude
-    ? throwLongitudeError(longitude)
-    : { latitude: latitude, longitude: longitude };
-
 const maximumLatitude = 60.84;
 const minimumLatitude = 49.9;
 const maximumLongitude = 1.77;
@@ -25,9 +18,16 @@ const throwRangeError = (value: number, type: "latitude" | "longitude") =>
   (() => {
     throw new RangeError(
       `The supplied ${type} value of ${value} does not fall within the range possible in the UK. The UK has a latitude range of ${
-        type == "latitude"
+        type === "latitude"
           ? `${minimumLatitude}-${minimumLatitude}`
           : `${minimumLongitude}-${maximumLongitude}`
       }`
     );
   })();
+
+export default (latitude: number, longitude: number): GeoCoordinates =>
+  latitude < minimumLatitude || latitude > maximumLatitude
+    ? throwLatitudeError(latitude)
+    : longitude < minimumLongitude || longitude > maximumLongitude
+    ? throwLongitudeError(longitude)
+    : { latitude: latitude, longitude: longitude };
