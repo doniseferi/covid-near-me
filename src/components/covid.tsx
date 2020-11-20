250b3b165daccdd7f53003ca640dd2b0100d8be1
Edonis Seferi <doni.seferi@gmail.com>
2020-11-20 23:26:17 +0000

    useeffect on covid

--- src/components/covid.tsx
+++ src/components/covid.tsx
@@ -1,40 +1,60 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { LocalInformation } from "../handler";
+import { Location } from "../location";
 
-const Covid = ({ location, covid }: LocalInformation) => {
+const Covid = ({ location: _location, covid: _covid }: LocalInformation) => {
+  const [location, setLocation] = useState<Location>(_location);
+  const [covid, setCovid] = useState<LocalInformation>({
+    location: _location,
+    covid: _covid,
+  });
+  useEffect(() => {
+    const covidApi = async () => {
+      if (location.geoCoordinates.latitude !== 0) {
+        console.log(location);
+        const url = `http://localhost:3000/api/covid?latitude=${_location.geoCoordinates.latitude}&longitude=${_location.geoCoordinates.longitude}`;
+        const result = (await axios.get<LocalInformation>(url)).data;
+        setCovid(result);
+        console.log({ coviddy: covid });
+      }
+    };
+    covidApi();
+  }, []);
   return (
     <dl>
       <dt>local authority</dt>
-      <dd>{location.code}</dd>
+      <dd>{_location.code}</dd>
 
       <dt>newCasesPublished</dt>
-      <dd>{covid.newCasesPublished}</dd>
+      <dd>{_covid.newCasesPublished}</dd>
 
       <dt>newCasesBySpecimen</dt>
-      <dd>{covid.newCasesBySpecimen}</dd>
+      <dd>{_covid.newCasesBySpecimen}</dd>
 
       <dt>newDeathsPublished</dt>
-      <dd>{covid.newDeathsPublished}</dd>
+      <dd>{_covid.newDeathsPublished}</dd>
 
       <dt>rateOfCumulativeCasesBySpecimenDate</dt>
-      <dd>{covid.rateOfCumulativeCasesBySpecimenDate}</dd>
+      <dd>{_covid.rateOfCumulativeCasesBySpecimenDate}</dd>
 
       <dt>cumulativeCasesBySpecimen</dt>
-      <dd>{covid.cumulativeCasesBySpecimen}</dd>
+      <dd>{_covid.cumulativeCasesBySpecimen}</dd>
 
       <dt>rateOfCumulativeDeathsBySpecimen</dt>
-      <dd>{covid.rateOfCumulativeDeathsBySpecimen}</dd>
+      <dd>{_covid.rateOfCumulativeDeathsBySpecimen}</dd>
 
       <dt>cumulativeDeathsBySpecimen</dt>
-      <dd>{covid.cumulativeDeathsBySpecimen}</dd>
+      <dd>{_covid.cumulativeDeathsBySpecimen}</dd>
 
       <dt>newDeaths28DaysByPublishDate</dt>
-      <dd>{covid.newDeaths28DaysByPublishDate}</dd>
+      <dd>{_covid.newDeaths28DaysByPublishDate}</dd>
 
       <dt>cumulativeDeaths28DaysByPublishDate</dt>
-      <dd>{covid.cumulativeDeaths28DaysByPublishDate}</dd>
+      <dd>{_covid.cumulativeDeaths28DaysByPublishDate}</dd>
 
       <dt>cumulativeDeaths28DaysByPublishDateRate</dt>
-      <dd>{covid.cumulativeDeaths28DaysByPublishDateRate}</dd>
+      <dd>{_covid.cumulativeDeaths28DaysByPublishDateRate}</dd>
     </dl>
   );
 };
