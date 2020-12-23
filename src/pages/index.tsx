00102e6f94ba71d2d0bb86b9c2bd6fea11ac7c1c
Edonis Seferi <doni.seferi@gmail.com>
2020-12-23 23:57:07 +0000

    minor css changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
+import Banner from "../components/Banner/Banner";
 import Container from "../components/Container/Container";
 import Covid from "../components/Covid/Covid";
 import useGeoCoordinates from "../hooks/useGeoCoordinates";
 import useCovidStatistics from "../hooks/useCovidStatistics";
-
+import React from "react";
 const Index = () => {
   const geoCoordinates = useGeoCoordinates();
   const localInformation = useCovidStatistics(geoCoordinates);
-  return <Container children={Covid(localInformation.statistics)} />;
+  return <Container>{Covid(localInformation.statistics)}</Container>;
 };
 
 export default Index;
