c44a64c33bd0349daa61f629a048195a8262b5a4
Edonis Seferi <doni.seferi@gmail.com>
2020-12-27 21:29:34 +0000

    ordering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
-import Header from "../components/Header/Header";
 import Container from "../components/Container/Container";
 import Covid from "../components/Covid/Covid";
+import Header from "../components/Header/Header";
 import useGeoCoordinates from "../hooks/useGeoCoordinates";
 import useCovidStatistics from "../hooks/useCovidStatistics";
 import React from "react";
+
 const Index = () => {
   const geoCoordinates = useGeoCoordinates();
   const localInformation = useCovidStatistics(geoCoordinates);
